dad038100badc61ca1294be31e8950a0fb9ff3e9
Shailesh Khote <shaileshkhote@gmail.com>
2020-12-18 21:08:06 +0530

    Registartion and login screen development

---
+++ app/src/main/java/com/project4n/healthpredictor/Login_Reg_Screen.java
@@ -0,0 +1,14 @@
+package com.project4n.healthpredictor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Reg_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__reg__screen);
+    }
+}
